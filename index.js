9e10ed957ac815920fc596fc8d214e347fca3504
Fatima-Anis <rude.fati@gmail.com>
2020-03-04 16:05:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+var name = 'syed hameed hussain'
+
+var numberofprasents = 40
+var numberofabsentds = 10
+var Totalstudents = numberofprasents + numberofabsentds
+
+alert(Totalstudents)
+var numner = 5
+var done = numner++;
+
+alert(numner)
+alert(done)
+var FirstName = 'hameed'
+var LastName = 'hussain'
+alert( ' hello' + ' ' + FirstName  + " " + LastName)
+
+
+var NumberOfBrothers = prompt('Number Of Brothers' , ' ' )
+var NumberOfSis = prompt('Nunber Of Sisters' , ' ' )
+var total = NumberOfBrothers - NumberOfSis
+alert('Total members in your family is' + total  )
+
+
+var yourname =prompt ('your name' , '') 
+if (yourname ==='hameed'){
+    alert('hi hameed')
+}
+else{
+    alert('hello bro')
+}
